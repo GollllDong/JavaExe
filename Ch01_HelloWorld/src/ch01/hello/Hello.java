5ce586702580688126e899284a162f8db90405ea
bitcamp <bitcamp@DESKTOP-V3TO1CL>
2024-01-24 12:35:20 +0900

    Hello -> 대한민국 Hello Java -> Hello, Java Hello World -> HelloWorld

--- Ch01_HelloWorld/src/ch01/hello/Hello.java
+++ Ch01_HelloWorld/src/ch01/hello/Hello.java
@@ -3,8 +3,9 @@ package ch01.hello;
 public class Hello {
 //	 프로그램 시작은 main메서드
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-		System.out.println("Hello Java");
-		System.out.println("Hello, Korea");
+		System.out.println("Helloworld");
+		System.out.println("Hello, Java");
+		System.out.println("Hello, 대한민국");
+		System.out.println("Hello, GollllDong");
 	}
 }
