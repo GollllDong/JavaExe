830eec142171ba162bc334564b9d3a93de3fdd18
bitcamp <bitcamp@DESKTOP-V3TO1CL>
2024-01-24 11:31:36 +0900

    Hello, Java 출력 추가

--- Ch01_HelloWorld/src/ch01/hello/Hello.java
+++ Ch01_HelloWorld/src/ch01/hello/Hello.java
@@ -3,6 +3,7 @@ package ch01.hello;
 public class Hello {
 //	 프로그램 시작은 main메서드
 	public static void main(String[] args) {
-		System.out.println("Hello world");		
+		System.out.println("Hello world");
+		System.out.println("Hello Java");
 	}
 }
