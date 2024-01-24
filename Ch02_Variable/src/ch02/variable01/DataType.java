d4e0529bbc89e65e9e758a56291c85cc4f2ca948
GollllDong <wjdqjatnwkd@naver.com>
2024-01-24 15:43:38 +0900

    name1 추가

---
+++ Ch02_Variable/src/ch02/variable01/DataType.java
@@ -0,0 +1,25 @@
+package ch02.variable01;
+
+public class DataType {
+	public static void main(String[] args) {
+		// 자료형 변수이름 = 값;
+		int num = 100;
+		double dnum = 3.14;
+		char ch = 'a';
+		String name = "홍길동";
+		boolean isRun = true;
+		String str = "a";
+		String name1 = "호우";
+		
+		System.out.println(num);
+		num = 999;
+		System.out.println(num);
+		System.out.println(dnum);
+		System.out.println(ch);
+		System.out.println(name);
+		System.out.println(name1);
+		System.out.println(isRun);
+		System.out.println(str);
+		
+	}
+}
