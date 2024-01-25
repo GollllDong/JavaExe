0c263b2ecfc6582572e666fec12291bed4d070ab
GollllDong <wjdqjatnwkd@naver.com>
2024-01-25 16:37:19 +0900

    Resolve4.java 1~10까지 2의배수 출력 while문

---
+++ Ch03_Control/src/ch03/while08/Resolve4.java
@@ -0,0 +1,13 @@
+package ch03.while08;
+
+public class Resolve4 {
+
+	public static void main(String[] args) {
+		int i = 0;
+		
+		while (i < 10) {
+			i += 2;
+			System.out.println("i의 짝수 : " + i);
+		}
+	}
+}
