dfe861e1b0253ccf72997048711b9b1425ad23b1
GollllDong <wjdqjatnwkd@naver.com>
2024-01-25 16:11:29 +0900

    WhileEx.java while 반복문사용

---
+++ Ch03_Control/src/ch03/while08/WhileEx.java
@@ -0,0 +1,15 @@
+package ch03.while08;
+
+public class WhileEx {
+
+	public static void main(String[] args) {
+		// while : ~동안
+		int i = 0;
+		while (i < 10) { // 초기식(시작 : 언제부터)
+			i++; 		// 증감식 (변화 : 얼마만큼)
+			System.out.print(i + ", "); // 실행문
+		}
+
+	}
+
+}
