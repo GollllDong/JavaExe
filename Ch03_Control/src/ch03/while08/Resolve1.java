c51d7f9d7d019b5f170f2a30111cf8020b5d7625
GollllDong <wjdqjatnwkd@naver.com>
2024-01-25 16:49:57 +0900

    초기값 수정 0 ->1 i < 100  ->  i <= 100

--- Ch03_Control/src/ch03/while08/Resolve1.java
+++ Ch03_Control/src/ch03/while08/Resolve1.java
@@ -3,10 +3,10 @@ package ch03.while08;
 public class Resolve1 {
 
 	public static void main(String[] args) {
-		int i = 0;
-		while ( i < 100) {
+		int i = 1;
+		while (i <= 100) {
+			System.out.print(i + ", ");
 			i++;
-			System.out.print(i + ", ");	
 		}
 	}
 }
