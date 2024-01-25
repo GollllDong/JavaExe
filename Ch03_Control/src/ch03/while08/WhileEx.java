6df5f87c05fcffb82d71eaae8ff280c718b0c78b
GollllDong <wjdqjatnwkd@naver.com>
2024-01-25 17:13:59 +0900

    while문 조건 변

--- Ch03_Control/src/ch03/while08/WhileEx.java
+++ Ch03_Control/src/ch03/while08/WhileEx.java
@@ -4,11 +4,11 @@ public class WhileEx {
 
 	public static void main(String[] args) {
 		// while : ~동안
-		int i = 0;
-		while (i < 10) { // 초기식(시작 : 언제부터)
-			i++; 		// 증감식 (변화 : 얼마만큼)
+		int i = 1;
+		while (i <= 10) { // 초기식(시작 : 언제부터)
 			System.out.print(i + ", "); // 실행문
 			System.out.println("Hi");
+			i++; 		// 증감식 (변화 : 얼마만큼)
 		}
 
 	}
