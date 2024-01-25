9f952884ea86f225f4fe5553541d370f2cce9fc6
GollllDong <wjdqjatnwkd@naver.com>
2024-01-25 16:51:19 +0900

    초기값 수정 i = 9 -> i = 10 i < 20  ->  i <= 20

--- Ch03_Control/src/ch03/while08/Resolve2.java
+++ Ch03_Control/src/ch03/while08/Resolve2.java
@@ -3,10 +3,10 @@ package ch03.while08;
 public class Resolve2 {
 
 	public static void main(String[] args) {
-		int i = 9;
-		while (i < 20) {
-			i++;
+		int i = 10;
+		while (i <= 20) {
 			System.out.print(i + ", ");
+			i++;
 		}
 	}
 }
