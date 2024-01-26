a36d96439c8d41a1d7dc510cd9c17e92b2e37e1c
GollllDong <wjdqjatnwkd@naver.com>
2024-01-26 10:57:58 +0900

    println, int i = 0 -> print, int i = 1 로 수정

--- Ch03_Control/src/ch03/for10/Resolve12.java
+++ Ch03_Control/src/ch03/for10/Resolve12.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Resolve12 {
 
 	public static void main(String[] args) {
-		System.out.println("입력 >>> ");
+		System.out.print("입력 >>> ");
 		Scanner sc = new Scanner(System.in);
 		int th = sc.nextInt();
-		for (int i = 0; i < th; i++) {
+		for (int i = 1; i <= th; i++) {
 			System.out.println("감사합니다.");
 		}
 		sc.close();
