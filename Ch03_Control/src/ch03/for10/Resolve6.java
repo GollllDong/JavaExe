0ef889d22fa78e68bd1dbc011bfbf08dd8e507ff
GollllDong <wjdqjatnwkd@naver.com>
2024-01-26 10:31:06 +0900

    do while문 내용 수정

--- Ch03_Control/src/ch03/for10/Resolve6.java
+++ Ch03_Control/src/ch03/for10/Resolve6.java
@@ -3,13 +3,14 @@ package ch03.for10;
 public class Resolve6 {
 
 	public static void main(String[] args) {
-		int i = 0;
+		int i = 1;
 		int sum = 0;
 		do {
-			System.out.println(i + sum);
-			i++;
 			sum += i;
-		}while(i < 10);
+			System.out.println(i);
+			i++;
+			
+		}while(i <= 10);
 		System.out.println("sum :" + sum);
 	}
 
