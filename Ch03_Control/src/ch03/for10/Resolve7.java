76f16fd585fb4833704f91d7a7bbb31223c3ed49
GollllDong <wjdqjatnwkd@naver.com>
2024-01-26 10:51:44 +0900

    num = 0 -> num = 1 수

--- Ch03_Control/src/ch03/for10/Resolve7.java
+++ Ch03_Control/src/ch03/for10/Resolve7.java
@@ -3,7 +3,7 @@ package ch03.for10;
 public class Resolve7 {
 
 	public static void main(String[] args) {
-		int num = 0;
+		int num = 1;
 		do {
 			if (num % 2 == 0) {
 				System.out.println(num);
