078c2f9a8f17edecf6308d0fd344dee823633bc3
GollllDong <wjdqjatnwkd@naver.com>
2024-01-26 11:02:22 +0900

    System.out.println("입력"); -> System.out.println("구구단의 출력하고 싶은 단 입력 >>> "); 수

--- Ch03_Control/src/ch03/for10/Resolve14.java
+++ Ch03_Control/src/ch03/for10/Resolve14.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Resolve14 {
 
 	public static void main(String[] args) {
-		System.out.println("입력 >>> ");
+		System.out.println("구구단의 출력하고 싶은 단 입력 >>> ");
 		Scanner sc = new Scanner(System.in);
 		int th = sc.nextInt();
 		for (int i = 1; i <= 9; i++) {
