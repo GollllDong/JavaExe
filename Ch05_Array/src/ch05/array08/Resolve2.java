595294ef9ff436fc84300074f44fa589a93a54e2
GollllDong <wjdqjatnwkd@naver.com>
2024-01-27 19:56:11 +0900

    2번째 for문 수정

--- Ch05_Array/src/ch05/array08/Resolve2.java
+++ Ch05_Array/src/ch05/array08/Resolve2.java
@@ -5,20 +5,24 @@ import java.util.Arrays;
 public class Resolve2 {
 
 	public static void main(String[] args) {
-		int[] num = {75, 20, 55, 62, 92};
+		int[] num = { 50, 10, 5, 60, 20 };
 		int max = 0;
 		int min = 0;
 
-		for(int i = 0; i < num.length-1; i++) {
-			if(num[i] > num[i+1]) {
-				int tmp = num[i];
-				num[i] = num[i+1];
-				num[i+1] = tmp;
-				System.out.println(Arrays.toString(num));
-				max = num[4];
-				min = num[0];
+		for (int i = 0; i < num.length - 1; i++) {
+			for (int j = i + 1; j < num.length; j++) {
+				if (num[i] > num[j]) {
+					int tmp = num[j];
+					num[j] = num[i];
+					num[i] = tmp;
+				}
+
 			}
+			min = num[0];
+			max = num[4];
 		}
+		
+		System.out.println(Arrays.toString(num));
 		System.out.println("최대값 : " + max);
 		System.out.println("최소값 : " + min);
 
