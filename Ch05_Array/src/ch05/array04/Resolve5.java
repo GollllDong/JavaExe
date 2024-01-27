f56442b5b3782f135ded7279984aaa0bd2189b39
GollllDong <wjdqjatnwkd@naver.com>
2024-01-27 22:45:12 +0900

    줄 맞춤

--- Ch05_Array/src/ch05/array04/Resolve5.java
+++ Ch05_Array/src/ch05/array04/Resolve5.java
@@ -7,13 +7,13 @@ public class Resolve5 {
 
 	public static void main(String[] args) {
 		// 5. int배열을 10개 생성하세요.
-		//	 사용자로부터 10개의 값을 입력받고
-		//	순서대로 출력하고  총합을 구하세요
+		// 사용자로부터 10개의 값을 입력받고
+		// 순서대로 출력하고 총합을 구하세요
 		int[] value = new int[10];
 		int sum = 0;
-		for(int i = 0; i <= value.length-1; i++) {
+		for (int i = 0; i <= value.length - 1; i++) {
 			System.out.print("값 입력 >>> ");
-			
+
 			Scanner sc = new Scanner(System.in);
 			value[i] = sc.nextInt();
 			sum += value[i];
