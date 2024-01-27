df9184816fdbb4e6e8d8031c65cb16c8728588b7
GollllDong <wjdqjatnwkd@naver.com>
2024-01-27 16:47:28 +0900

    Resolve2.java max, min 출력

--- Ch05_Array/src/ch05/array08/Resolve2.java
+++ Ch05_Array/src/ch05/array08/Resolve2.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Resolve2 {
 
 	public static void main(String[] args) {
-		int[] num = {95, 80, 25, 60, 50};
+		int[] num = {75, 20, 55, 62, 92};
 		int max = 0;
 		int min = 0;
 
@@ -14,12 +14,9 @@ public class Resolve2 {
 				int tmp = num[i];
 				num[i] = num[i+1];
 				num[i+1] = tmp;
-				max = num[i+1];
-			}else if (num[i] < num[i+1]) {
-				int tmp = num[i+1];
-				num[i+1] = num[i];
-				num[i] = tmp;
-				min = num[i];
+				System.out.println(Arrays.toString(num));
+				max = num[4];
+				min = num[0];
 			}
 		}
 		System.out.println("최대값 : " + max);
