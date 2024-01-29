7a10fa9d681f6d67f16c9a5b8219dd0511223afa
GollllDong <wjdqjatnwkd@naver.com>
2024-01-29 12:09:01 +0900

    값 바꾸

---
+++ Ch05_Array/src/ch05/array08/SwapEx.java
@@ -0,0 +1,22 @@
+package ch05.array08;
+
+public class SwapEx {
+
+	public static void main(String[] args) {
+		
+		// 2개 변수의 값을 바꾸려면
+		int a = 10;
+		int b = 20;
+		System.out.println("a : "+a);
+		System.out.println("b : "+b);
+		int tmp = a;
+		a = b;
+		b = tmp;
+		
+		System.out.println("a : "+a);
+		System.out.println("b : "+b);
+		
+
+	}
+
+}
