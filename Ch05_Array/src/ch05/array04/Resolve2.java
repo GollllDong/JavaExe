65fa34ca0f8dd22b66886862b9ba27113b6f7397
GollllDong <wjdqjatnwkd@naver.com>
2024-01-29 11:05:57 +0900

    int i = names.length-1; i <= names.length; 수정

--- Ch05_Array/src/ch05/array04/Resolve2.java
+++ Ch05_Array/src/ch05/array04/Resolve2.java
@@ -4,14 +4,16 @@ public class Resolve2 {
 
 	public static void main(String[] args) {
 		//1. String 배열을 10명의 이름으로 초기화하고 순서대로 출력하세요
-		String[] names = {"정범수", "안은선", "김원종", "김경식", "엄기은", "김동규", "배현석", "손흥민", "김민재", "이강인"};
-		for (int i = 9; i < names.length; i--) {
+		String[] names = {"정범수", "봉준호", "제이팍", "김모군", "이모군", "박모군", "황모군", "손흥민", "김민재", "이강인"};
+		for (int i = names.length-1; i <= names.length; i--) {
 			System.out.println((i+1)+"번째 : "+names[i]);
+			
 			
 			if(names[i] == "정범수") {
 				break;
 			}
 		}
+		System.out.println();
 	
 	}
 
