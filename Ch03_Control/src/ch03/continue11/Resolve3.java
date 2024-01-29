50e2369bbb265ec2f592f997fcc747aeb86e5d53
GollllDong <wjdqjatnwkd@naver.com>
2024-01-29 10:17:20 +0900

    println -> print value1, value2위치 main메서드의 지역변수로 수

--- Ch03_Control/src/ch03/continue11/Resolve3.java
+++ Ch03_Control/src/ch03/continue11/Resolve3.java
@@ -6,38 +6,38 @@ public class Resolve3 {
 
 	public static void main(String[] args) { // p 139 7번
 		int account = 0;
+		int value1 = 0;
+		int value2 = 0;
 		while(true) {
 			
-			int value1 = 0;
-			int value2 = 0;
-			int value3 = 0;
+			
 			System.out.println("---------------------------------");
 			System.out.println("1.예금 | 2.출금 | 3. 잔고 | 4. 종료");
 			System.out.println("---------------------------------");
 			
-			System.out.println("선택 > ");
+			System.out.print("선택 > ");
 			Scanner sc = new Scanner(System.in);
 			String value = sc.nextLine();
 			
 			
 			if(value.equals("1")) {
 				System.out.println("예금기능");
-				System.out.println("예금액 : ");
+				System.out.print("예금액 : ");
 				value1= sc.nextInt();
 				account += value1;
 			}else if (value.equals("2")) {
 				System.out.println("출금기능");
-				System.out.println("출금액 : ");
+				System.out.print("출금액 : ");
 				value2= sc.nextInt();
 				account -= value2;
 			}
 			else if (value.equals("3")) {
-				System.out.println("잔고확인");
+				System.out.print("잔고확인 : ");
 				System.out.println(account);
 			}
 			else if (value.equals("4")) {
 				System.out.println("종료");
-				System.out.println(account);
+				break;
 			}
 			
 		}
