3ed6dda3ff76a0402d14632c7b8845782f590fa7
GollllDong <wjdqjatnwkd@naver.com>
2024-01-29 09:36:43 +0900

    p138 7번

--- Ch03_Control/src/ch03/continue11/Resolve1.java
+++ Ch03_Control/src/ch03/continue11/Resolve1.java
@@ -2,7 +2,7 @@ package ch03.continue11;
 
 public class Resolve1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { // p138 3번
 		System.out.println("3의 배수 총합 ");
 		int sum = 0;
 		for (int i = 1; i <= 100; i++) {
