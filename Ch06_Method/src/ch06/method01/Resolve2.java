16cbd1c03b7d5324be077e189feabc3aada0d57a
GollllDong <wjdqjatnwkd@naver.com>
2024-01-29 17:04:31 +0900

    Resolve 문제 해결

--- Ch06_Method/src/ch06/method01/Resolve2.java
+++ Ch06_Method/src/ch06/method01/Resolve2.java
@@ -4,25 +4,13 @@ import java.util.Scanner;
 
 public class Resolve2 {
 	
-	static void pr(int value1, int value2) {
-		if(value1 < 0) {
-			value1 = -value1 + value1; 
-		}else if (value2 < 0) {
-			value2 = -value2 + value2; 
-		}
-		int result = value1;
-		int result2 = value2;
-		System.out.println(result);
-		System.out.println(result);
-		if (result > result2) {
-			int a = result - result2;
-			System.out.println(a);
-		}
-		else if (result < result2) {
-			int b = result2 - result;
-			System.out.println(b);
-		}
+	public static int absoulte(int value1, int value2) {
+		int tmp = value1 - value2;
+		int result = (tmp < 0) ? -(tmp): tmp;
+		return result;
 	}
+	
+	
 	
 	public static void main(String[] args) {
 //		두 개의 정수를 받아서, 두 수의 차의 절대값을 계산하여 출력하는 메서드를 만들고
@@ -31,11 +19,13 @@ public class Resolve2 {
 		Scanner sc = new Scanner(System.in);
 		
 
-		System.out.println("1번째 값 입력 >>> ");
+		System.out.print("1번째 값 입력 >>> ");
 		int value1 = sc.nextInt();
-		System.out.println("2번째 값 입력 >>> ");
+		System.out.print("2번째 값 입력 >>> ");
 		int value2 = sc.nextInt();
-		pr(value1, value2);
+		int result = absoulte(value1, value2);
+		System.out.println(result);
+		
 		
 		sc.close();
 	}
