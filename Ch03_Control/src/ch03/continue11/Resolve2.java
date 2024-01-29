f717bd1132e7fc756772bc3249bf146677bb694e
GollllDong <wjdqjatnwkd@naver.com>
2024-01-29 09:37:23 +0900

    p139 6번

--- Ch03_Control/src/ch03/continue11/Resolve2.java
+++ Ch03_Control/src/ch03/continue11/Resolve2.java
@@ -2,7 +2,7 @@ package ch03.continue11;
 
 public class Resolve2 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { // p139 6번
 		for (int i = 1; i <= 5; i++) {
 			for(int j = 1; j <= i; j++) {
 				System.out.print("*");
