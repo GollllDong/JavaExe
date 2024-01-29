12d6706d266bb8b1fe9623972da5cfbaaacac843
GollllDong <wjdqjatnwkd@naver.com>
2024-01-29 11:11:41 +0900

    for문 및 avg 자료형 double로 변환

--- Ch05_Array/src/ch05/array04/Resolve3.java
+++ Ch05_Array/src/ch05/array04/Resolve3.java
@@ -8,16 +8,20 @@ public class Resolve3 {
 		// 3. 7명의 학생의 학점을 입력받고 총점과 평균을 출력하세요
 		int[] value = new int[7];
 		int sum = 0;
-		int total, avg = 0;
+		int total = 0;
+		double avg = 0;
+		Scanner sc = new Scanner(System.in);
+		
 		for(int i = 1; i <= value.length; i++) {			
 			System.out.print("학점 입력 >>> ");
-			Scanner sc = new Scanner(System.in);
 			sum += sc.nextInt();
 		}
 		total = sum;
-		avg = total/value.length;
+		avg = (double)total/value.length;
 		System.out.println("총점 : " + total);
 		System.out.println("평균 : " + avg);
+
+		sc.close();
 	}
 
 }
