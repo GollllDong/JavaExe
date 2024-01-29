aba8af31b07f6bfbfa4caf0914c5714f62a3a388
GollllDong <wjdqjatnwkd@naver.com>
2024-01-29 16:47:40 +0900

    Resolve 문제 해결

--- Ch06_Method/src/ch06/method01/Resolve1.java
+++ Ch06_Method/src/ch06/method01/Resolve1.java
@@ -4,18 +4,14 @@ import java.util.Scanner;
 
 public class Resolve1 {
 	
-	public static int big(int tmp1, int tmp2) {
-		if (tmp1 > tmp2) {
-			return tmp1;
-		}
-		else {
-			return tmp2; // 여기 부분에서 작은 값 리턴
-		}
-		
+	public static int big(int tmp1) {
+		System.out.println("큰 값 : " + tmp1);
+		return tmp1;
 	}
-//	public static int small(int tmp1) {
-//		
-//	}
+	public static int small(int tmp2) {
+		System.out.println("작은 값 : " + tmp2);
+		return tmp2;
+	}
 
 	public static void main(String[] args) {
 		// 2개의 정수를 입력받고 큰 값을 반환하는 메서드와
@@ -26,10 +22,16 @@ public class Resolve1 {
 		int tmp1 = sc.nextInt();
 		System.out.println("2번째 값 입력 : ");
 		int tmp2 = sc.nextInt();
-		int result = big(tmp1, tmp2);
-		System.out.println(result);
+		
+		if(tmp1 > tmp2) {
+			big(tmp1);
+		}else if (tmp1 < tmp2) {
+			small(tmp2);
+		}
+		else {
+			System.out.println("잘못된 입력값입니다.");
+		}
 	
-
 	}
 
 }
