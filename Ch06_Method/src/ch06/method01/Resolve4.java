6bd4c8ce9fdc8c1fa2e9dcc7c4472fed61f89f21
GollllDong <wjdqjatnwkd@naver.com>
2024-01-29 16:38:43 +0900

    Resolve 문제 해결

---
+++ Ch06_Method/src/ch06/method01/Resolve4.java
@@ -0,0 +1,27 @@
+package ch06.method01;
+
+import java.util.Scanner;
+
+public class Resolve4 {
+
+	public static void gugudan(int tmp) {
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(tmp + "*" + i + "=" + tmp*i);
+		}
+	}
+
+	public static void main(String[] args) {
+//		gugudan(3);
+//		   
+//		   구구단 메서드를 void 리턴형으로 제작하세요.
+//		  3을 입력하면 3단이 출력됩니다.
+		Scanner sc = new Scanner(System.in);
+		System.out.print("출력할 단 입력 : ");
+		int tmp = sc.nextInt();
+		
+
+		gugudan(tmp);
+
+	}
+
+}
