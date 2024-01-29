fa8afe9d8024a84a525b05389ee24488440f30d9
GollllDong <wjdqjatnwkd@naver.com>
2024-01-29 10:19:28 +0900

    공백 제

--- Ch03_Control/src/ch03/continue11/Resolve3.java
+++ Ch03_Control/src/ch03/continue11/Resolve3.java
@@ -8,40 +8,35 @@ public class Resolve3 {
 		int account = 0;
 		int value1 = 0;
 		int value2 = 0;
-		while(true) {
-			
-			
+		while (true) {
+
 			System.out.println("---------------------------------");
 			System.out.println("1.예금 | 2.출금 | 3. 잔고 | 4. 종료");
 			System.out.println("---------------------------------");
-			
+
 			System.out.print("선택 > ");
 			Scanner sc = new Scanner(System.in);
 			String value = sc.nextLine();
-			
-			
-			if(value.equals("1")) {
+
+			if (value.equals("1")) {
 				System.out.println("예금기능");
 				System.out.print("예금액 : ");
-				value1= sc.nextInt();
+				value1 = sc.nextInt();
 				account += value1;
-			}else if (value.equals("2")) {
+			} else if (value.equals("2")) {
 				System.out.println("출금기능");
 				System.out.print("출금액 : ");
-				value2= sc.nextInt();
+				value2 = sc.nextInt();
 				account -= value2;
-			}
-			else if (value.equals("3")) {
+			} else if (value.equals("3")) {
 				System.out.print("잔고확인 : ");
 				System.out.println(account);
-			}
-			else if (value.equals("4")) {
+			} else if (value.equals("4")) {
 				System.out.println("종료");
 				break;
 			}
-			
+
 		}
-		
 
 	}
 
