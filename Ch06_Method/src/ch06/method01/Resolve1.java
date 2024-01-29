968fcf3be1db70c32a9f7a65bf3be2303aabefa2
GollllDong <wjdqjatnwkd@naver.com>
2024-01-29 21:31:00 +0900

    return문 및 값 입력 수정

--- Ch06_Method/src/ch06/method01/Resolve1.java
+++ Ch06_Method/src/ch06/method01/Resolve1.java
@@ -4,13 +4,12 @@ import java.util.Scanner;
 
 public class Resolve1 {
 	
-	public static int big(int tmp1) {
-		System.out.println("큰 값 : " + tmp1);
-		return tmp1;
+	public static int big(int tmp1, int tmp2) {
+		return tmp1 > tmp2 ? tmp1 : tmp2;
+		
 	}
-	public static int small(int tmp2) {
-		System.out.println("작은 값 : " + tmp2);
-		return tmp2;
+	public static int small(int tmp1, int tmp2) {
+		return tmp1 < tmp2 ? tmp1 : tmp2;
 	}
 
 	public static void main(String[] args) {
@@ -18,19 +17,25 @@ public class Resolve1 {
 		// 작은 값을 반환하는 메서드를 작성하고 사용하세요
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("1번째 값 입력 : ");
+		System.out.print("1번째 값 입력 : ");
 		int tmp1 = sc.nextInt();
-		System.out.println("2번째 값 입력 : ");
+		System.out.print("2번째 값 입력 : ");
 		int tmp2 = sc.nextInt();
 		
-		if(tmp1 > tmp2) {
-			big(tmp1);
-		}else if (tmp1 < tmp2) {
-			small(tmp2);
-		}
-		else {
-			System.out.println("잘못된 입력값입니다.");
-		}
+		
+		System.out.println("큰 값 : "+big(tmp1,tmp2));
+		System.out.println("작은 값 : " + small(tmp1,tmp2));
+		
+//		if(tmp1 > tmp2) { // 1번째 값이 더 클 경우
+//			big(tmp1);    // 1번째 값을 big 메소드에 할당
+//			small(tmp2);  // 2번째 값을 small 메소드에 할당
+//		}else if (tmp1 < tmp2) { // 2번째 값이 더 클 경우
+//			small(tmp1); // 1번째 값을 small 메소드에 할당
+//			big(tmp2);  // 2번째 값을 big 메소드에 할당
+//		}
+//		else {
+//			System.out.println("잘못된 입력값입니다.");
+//		}
 	
 	}
 
