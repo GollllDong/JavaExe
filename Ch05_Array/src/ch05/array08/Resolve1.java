e176e883277cb687922dca066433cc4dd0fc3d9d
GollllDong <wjdqjatnwkd@naver.com>
2024-01-29 09:33:40 +0900

    System.out.println(Arrays.toString(num)); - > System.out.println("정렬 전 : "+Arrays.toString(num)); - > System.out.println("정렬 후 : "+Arrays.toString(num));

--- Ch05_Array/src/ch05/array08/Resolve1.java
+++ Ch05_Array/src/ch05/array08/Resolve1.java
@@ -7,9 +7,9 @@ public class Resolve1 {
 	public static void main(String[] args) {
 		int[] num = {95, 80, 25, 60, 50};
 
-		System.out.println(Arrays.toString(num));
+		System.out.println("정렬 전 : "+Arrays.toString(num));
 		Arrays.sort(num);
-		System.out.println(Arrays.toString(num));
+		System.out.println("정렬 후 : "+Arrays.toString(num));
 		int max = num[4];
 		int min = num[0];
 		System.out.println("최대값 : "+ max);
