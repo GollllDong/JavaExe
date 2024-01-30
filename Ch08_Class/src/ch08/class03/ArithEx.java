fe37aa2b7864990cbe55e85fb2c25894853a5b17
GollllDong <wjdqjatnwkd@naver.com>
2024-01-30 14:52:04 +0900

    static 메서드 구현

---
+++ Ch08_Class/src/ch08/class03/ArithEx.java
@@ -0,0 +1,23 @@
+package ch08.class03;
+
+/* static 메서드는 객체를 만들지 않아도 바로 사용이 가능하다. (클래스 메서드)
+ * 
+ * */
+
+public class ArithEx {
+	public static void main(String[] args) {
+		int a = 10, b = 5;
+		int result = 0;
+		result = Arith.add(a, b);
+		System.out.println(result);
+		
+		result = Arith.sub(a, b);
+		System.out.println(result);
+		
+		result = Arith.mul(a, b);
+		System.out.println(result);
+		
+		result = Arith.div(a, b);
+		System.out.println(result);
+	}
+}
