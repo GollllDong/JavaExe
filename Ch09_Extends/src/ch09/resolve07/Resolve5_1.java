1b64fc802741b67f51a05001d2a9e0519c4284c9
GollllDong <wjdqjatnwkd@naver.com>
2024-01-31 23:47:22 +0900

    Resolve1.java 정수형 값 2개 입력받고 String형으로 연산자까지 입력받은 후 연산

---
+++ Ch09_Extends/src/ch09/resolve07/Resolve5_1.java
@@ -0,0 +1,26 @@
+package ch09.resolve07;
+
+public class Resolve5_1 {
+	private int bead1;
+	private int bead2;
+
+	public Resolve5_1(int bead1, int bead2) {
+		this.bead1 = bead1;
+		this.bead2 = bead2;
+	}
+	public void FirstGame() {
+		this.bead1 += 2;
+		this.bead2 -= 2;
+	}
+	public void SecondGame() {
+		this.bead2 += 7;
+		this.bead1 -= 7;
+	}
+	public void View_Info() {
+		int tmp1 = this.bead1;
+		int tmp2 = this.bead2;
+		System.out.println("첫번째 어린이 구슬 개수 : " + tmp1);
+		System.out.println("두번째 어린이 구슬 개수 : " + tmp2);
+	}
+
+}
