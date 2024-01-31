9a28381d24c89573a396e9fd986959cd9cf5b47d
GollllDong <wjdqjatnwkd@naver.com>
2024-01-31 17:10:31 +0900

    접근제한자의 종류

---
+++ Ch09_Extends/src/ch09/extends05/Child.java
@@ -0,0 +1,24 @@
+package ch09.extends05;
+
+public class Child extends Father {
+	
+	private float day = 365 + 1.0f / 4;
+	
+	Child() {
+		System.out.println("Child 생성자"); 
+	}
+	
+	Child(int score, long money, float day) {
+		// 위의 부모의 매개변수 생성자를 호출해준다.
+		super(score, money); {
+		this.day = day;
+		System.out.println("Child 매개변수 생성자");
+		}
+	}
+	
+	void play() {
+		System.out.println("인생이 즐겁다!!");
+	}
+	
+
+}
