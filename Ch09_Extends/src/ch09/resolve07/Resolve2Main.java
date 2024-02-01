9900d119b6a6bb9519bc12721ac0e40e9d93913c
GollllDong <wjdqjatnwkd@naver.com>
2024-02-01 10:31:55 +0900

    생성자 값 수정

--- Ch09_Extends/src/ch09/resolve07/Resolve2Main.java
+++ Ch09_Extends/src/ch09/resolve07/Resolve2Main.java
@@ -3,14 +3,14 @@ package ch09.resolve07;
 
 public class Resolve2Main {
 	public static void main(String[] args) {
-		Resolve2 tri1 = new Resolve2(7,5);
+		Resolve2 tri1 = new Resolve2(10,10);
 		Resolve2 tri2 = new Resolve2(9,6);
 		
 		System.out.println("1번 삼각형의 넓이 : "+tri1.TriArea());
 		System.out.println("2번 삼각형의 넓이 : "+tri2.TriArea());
 		
-		tri1.setDownLine(15);
-		tri2.setHeight(5);
+		tri1.setDownLine(20);
+		tri2.setHeight(20);
 		
 		
 		System.out.println("1번 삼각형의 넓이 : "+tri1.TriArea());
