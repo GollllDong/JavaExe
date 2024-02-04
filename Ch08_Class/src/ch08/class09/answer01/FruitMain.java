52d553e754d29c96484a817b16d2fda9abbf6c94
GollllDong <wjdqjatnwkd@naver.com>
2024-02-04 16:01:11 +0900

    Scanner 클래스 미사용으로 인한 삭제

--- Ch08_Class/src/ch08/class09/answer01/FruitMain.java
+++ Ch08_Class/src/ch08/class09/answer01/FruitMain.java
@@ -1,12 +1,8 @@
 package ch08.class09.answer01;
 
-import java.util.Scanner;
-
 public class FruitMain {
 
 	public static void main(String[] args) {
-		Scanner sc = null;
-		
 		FruitSeller seller = new FruitSeller(20);
 		FruitBuyer buyer = new FruitBuyer(10000);
 
