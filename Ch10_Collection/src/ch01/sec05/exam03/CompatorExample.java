7a68758e6f9ef20d53229c0a38e84fd23acb0419
GollllDong <wjdqjatnwkd@naver.com>
2024-02-06 17:52:45 +0900

    주석 추가 및 조건문 변경

--- Ch10_Collection/src/ch01/sec05/exam03/CompatorExample.java
+++ Ch10_Collection/src/ch01/sec05/exam03/CompatorExample.java
@@ -10,7 +10,14 @@ public class CompatorExample  {
 		// 객체 저장
 	    treeSet.add(new Person("홍길동",45));
 	    treeSet.add(new Person("김자바", 25));
-	    treeSet.add(new Person("박지원",31));
+	    treeSet.add(new Person("차돌박이",31));
+	    treeSet.add(new Person("임꺽정",51));
+	    treeSet.add(new Person("정지민",63));
+	    treeSet.add(new Person("머털도사",75));
+	    treeSet.add(new Person("둘리",801));
+	    treeSet.add(new Person("마이콜",55));
+	    treeSet.add(new Person("홍길동",564));
+	    treeSet.add(new Person("기영이",18));
 	    
 	    // 객체를 하나씩 가져오기
 
--- Ch10_Collection/src/ch01/sec05/exam03/Person.java
+++ Ch10_Collection/src/ch01/sec05/exam03/Person.java
@@ -11,11 +11,14 @@ public class Person implements Comparable<Person> {
 
 	@Override
 	public int compareTo(Person o) {
-		if (age < o.age)
-			return -1;
-		else if (age == o.age)
-			return 0;
-		else
-			return 1;
+		return name.compareTo(o.name); // 이름 앞자리의 ㄱㄴㄷㄹㅁㅂㅅ 순서로 나오게
+//		if (age < o.age) // 오름차 순, 내림차 순으로 만들어 줄 수 있다.
+//			//if (name.hashCode() < o.name.hashCode()) 처럼 해시코드로도 가능하다.
+//			return -1;
+//		else if (age == o.age)
+//			return 0;
+//		else
+//			return 1;
+//	}
 	}
 }
