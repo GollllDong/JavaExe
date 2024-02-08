6f8649f1bc4acedf8e08a1d9636574182f1c0d2c
GollllDong <wjdqjatnwkd@naver.com>
2024-02-08 11:23:08 +0900

    주석 추가

--- Ch13_Generic/src/ch13/sec02/exam01/GenericExample.java
+++ Ch13_Generic/src/ch13/sec02/exam01/GenericExample.java
@@ -1,8 +1,5 @@
 package ch13.sec02.exam01;
 
-import ch13.sec02.exam01.Product;
-import ch13.sec02.exam01.Tv;
-
 public class GenericExample {
 	public static void main(String[] args) {
 		
@@ -16,6 +13,8 @@ public class GenericExample {
 		//Getter 리턴값은 TV와 String이 됨
 		Tv tv = product1.getKind();
 		String tvModel = product1.getModel();
+		System.out.println(tv);
+		System.out.println(tvModel);
 		
 		// K는 Car로 대체, M은 String으로 대체
 		Product<Car, String> product2 = new Product<>();
@@ -27,5 +26,8 @@ public class GenericExample {
 		// Getter 리턴값은 Car와 String이 됨
 		Car car = product2.getKind();
 		String carModel = product2.getModel();
+		System.out.println(car);
+		System.out.println(carModel);
+		
 	}
 }
