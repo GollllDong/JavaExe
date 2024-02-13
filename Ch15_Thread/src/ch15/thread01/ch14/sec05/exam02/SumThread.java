add0455f928ee9864975503a6092598d4e9f2836
GollllDong <wjdqjatnwkd@naver.com>
2024-02-13 11:57:06 +0900

    Ch15_Thread 생성 및 예제 작성

---
+++ Ch15_Thread/src/ch15/thread01/ch14/sec05/exam02/SumThread.java
@@ -0,0 +1,18 @@
+package ch15.thread01.ch14.sec05.exam02;
+
+public class SumThread extends Thread {
+		private long sum;
+		
+		public long getSum(){
+			return sum;
+		}
+		public void setSum(long sum) {
+			this.sum = sum;
+		}
+		@Override
+		public void run() {
+			for(int i=1; 1<=100; i++) {
+				sum+=i;
+			}
+		}
+}
---
+++ Ch15_Thread/src/ch15/thread01/ch14/sec07/exam03/InterruptedExample.java
@@ -0,0 +1,14 @@
+package ch15.thread01.ch14.sec07.exam03;
+
+public class InterruptedExample {
+	public static void main(String[] args) {
+		Thread Thread = new PrintThread();
+		Thread.start();
+		
+		try {
+			Thread.sleep(1000);
+		}catch(InterruptedException e) {}
+		
+		Thread.interrupt();;
+	}
+}
