f765634b7b06fb34356b6b61c6cea0930448a245
GollllDong <wjdqjatnwkd@naver.com>
2024-02-16 14:56:19 +0900

    JsonChatServer 구현중

--- Ch19_Network/src/ch19/server/jsonchatserver05/JsonChatServer.java
+++ Ch19_Network/src/ch19/server/jsonchatserver05/JsonChatServer.java
@@ -1,6 +1,7 @@
 package ch19.server.jsonchatserver05;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -11,18 +12,18 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Hashtable;
 
-/*  1) EchoServer
- *  - 서버소켓의 동작방식
- *  - 2개 이상의 소켓 필요 (서버소켓(accept), 통신소켓(send/recv))
- *  2) EchoMultiThreadServer
- *  - 병렬 처리를 위해 Clinet 1개당 Thread 1개를 배정
- *  3) JsonChatServer
- *  - 다양한 업무 처리를 위해서 JSon 포맷 사용(패킷)
- *  - 채팅은 1사람한테 수신된 데이터를 다른 사람한테 전달해야 한다.
- *  그러므로 사용자 등록/관리가 필요하다. (HashTable = HashMap + 동기화)
- * 
- * */
+import org.json.JSONObject;
 
+/* 1) EchoServer
+ *   - 서버소켓의 동작방식
+ *   - 2개 이상의 소켓 필요(서버소켓(accpt), 통신소켓(send/recv))
+ * 2) EchoMultiThreadServer
+ * 	 - 병렬 처리를 위해 Client 1개당 Thread 1개를 배정
+ * 3) JsonChatServer
+ *   - 다양한 업무 처리를 위해서 Json 포맷 사용(패킷)
+ *   - 채팅은 1사람한테 수신된 데이터를 다른 사람한테 전달해야 한다.
+ *     그러므로 사용자 등록/관리가 필요하다. (HashTable=HashMap+동기화)
+ * */
 public class JsonChatServer {
 	public static void main(String[] args) {
 		final int PORT = 9000;
@@ -31,10 +32,11 @@ public class JsonChatServer {
 		try {
 			ServerSocket serverSocket = new ServerSocket(PORT);
 			String mainThreadName = Thread.currentThread().getName();
-			/* main thread는 계속 accept() 처리를 담당한다. */
+			/* main thread는 계속 accept()처리를 담당한다 */
 			while (true) {
 				System.out.printf("[서버-%s] Client접속을 기다립니다...", mainThreadName);
 				Socket socket = serverSocket.accept();
+
 				/* worker thread는 Client와의 통신을 담당한다. */
 				WorkerThread wt = new WorkerThread(socket, clientHt);
 				wt.start();
@@ -48,33 +50,63 @@ public class JsonChatServer {
 
 /* [App 프로토콜 정의]
  * 1) 데이터는 '문자열' 데이터로 한다.
- * 2) '문자열' 데이터는 json을 변환한 것으로 한다.
+ * 2) '문자열'데이터는 json을 변환한 것으로 한다.
  * 3) json 명령 패킷 종류
- * 		- {}내의 데이터의 값을 의미한다.
- * 	3-1) id등록
- * 		cmd : ID
- * 		id : {id값}
- * 	3-2) 사칙연산
- * 		cmd : Arith
- * 		id : {id값}
- * 		op : {연산자}
- * 		val1 : {첫번째값}
- * 		val2 : {두번째값}
- * 	3-3) 전체 채팅
- * 		cmd : ALLCHAT
- * 		id : {id값}
- * 		youid : 미ㅣ
- * 		msg : {문자메시지}
- * 		
- *  3-4) 1:1 채팅 
- *  	cmd : ONECHAT
- *  	id : {id값}
- * 		youid : {상대id}
- * 		msg : {문자메시지}
+ *    - {}내의 데이터를 값을 의미한다.
+ *   3-1) id등록
+ *     [요청]
+ *     cmd:ID
+ *     id:{id값}
+ *     
+ *     [응답]
+ *     cmd:ID
+ *     ack:ok(성공), fail(실패)
+ *   3-2) 사칙 연산
+ *     [요청]
+ *     cmd:ARITH
+ *     id:{id값}
+ *     op:{연산자}
+ *     val1:{첫번째값}
+ *     val2:{두번째값}
+ *     
+ *     [응답]
+ *     cmd:ARITH
+ *     ack:{결과값}
+ *     
+ *   3-3) 전체 채팅
+ *     [요청]
+ *     cmd:ALLCHAT
+ *     id:{id값}
+ *     youid:all
+ *     msg:{문자메시지}
+ *     
+ *     [응답]
+ *     cmd:ID
+ *     ack:ok(성공), fail(실패)
+ *     
+ *     [전송]
+ *     cmd:BROADCHAT
+ *     id:{id값}
+ *     msg:{문자메시지}
+ *   3-4) 1:1 채팅
+      [요청]
+ *     cmd:ONECHAT
+ *     id:{id값}
+ *     youid:{상대id}
+ *     msg:{문자메시지}
+ *     
+ *     [응답]
+ *     cmd:ID
+ *     ack:ok(성공), fail(실패)
+ *     
+ *     [전송]
+ *     cmd:UNIQCHAT
+ *     id:{id값}
+ *     msg:{문자메시지}
  * */
 
 // json 라이브러리 다운로드 후 등록
-// https://github.com/stleary/JSON-java
+//https://github.com/stleary/JSON-java
 
 class WorkerThread extends Thread {
 	private Socket socket;
@@ -95,18 +127,95 @@ class WorkerThread extends Thread {
 			PrintWriter pw = new PrintWriter(new OutputStreamWriter(out));
 			BufferedReader br = new BufferedReader(new InputStreamReader(in));
 			
-			String line;
 			while(true) {
-				// client가 json오브젝트를 string으로 변환해서 보낸 것을 수
-				line = br.readLine();
-				if(line == null) {
+				/*client가 json오브젝트를 string으로 변환해서 보낸 것을 수신*/
+				String line = br.readLine();
+				if(line == null)
 					break;
-				}
-				/*json패킷을 해석해서 알맞은 처리를 한다.*/
-				JSONOBJECT packetObj = new JSONObject(line);
+				/*json패킷을 해석해서 알맞은 처리를 한다.
+				 * 문자열 -> JSONObject 변환 -> cmd를 해석해서 어떤 명령인지?
+				 * */
+				JSONObject packetObj = new JSONObject(line);
+				processPacket(packetObj);
+				
 			}
 		} catch (Exception e) {
 			System.out.printf("<서버-%s>%s\n", getName(), e.getMessage());
 		}
 	}
+	
+	private void processPacket(JSONObject packetObj) throws IOException {
+		JSONObject ackObj = new JSONObject();
+		String cmd = packetObj.getString("cmd");
+		
+		if(cmd.equals("ID")) {
+			// 클라이언트 요청 처리
+			String id = packetObj.getString("id");
+			ht.put(id, this.socket);	// 해시테이블에 id와 socket을 등록		
+			// 응답			
+			ackObj.put("cmd", id);
+			ackObj.put("ack", "ok");
+			// Json Obj -> 문자열
+			String ack = ackObj.toString();
+			// 클라이언트한테 전송
+			OutputStream out = this.socket.getOutputStream();
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(out));
+			pw.println(ack);
+			pw.flush();
+		}else if(cmd.equals("ARITH")) {
+			// 요청 처리
+			String id = packetObj.getString("id");
+			String op = packetObj.getString("op");
+			String val1 = packetObj.getString("val1");
+			String val2 = packetObj.getString("val2");
+			double v1 = Double.parseDouble(val1);
+			double v2 = Double.parseDouble(val2);
+			double result = arith(op, v1, v2);
+			// 응답
+			ackObj.put("cmd", "ARITH");
+			ackObj.put("ack", Double.toString(result));
+			// Json Obj -> 문자열
+			String ack = ackObj.toString();
+			// 클라이언트한테 전송
+			OutputStream out = this.socket.getOutputStream();
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(out));
+			pw.println(ack);
+			pw.flush();
+		}else if(cmd.equals("ALLCHAT")) {
+			
+		}else if(cmd.equals("ONECHAT")) {
+			
+		}
+		
+		
+
+	}
+	private double arith(String op, double val1, double val2) {
+		double result=0.;
+		switch(op) {
+		case "+":
+			result = val1+val2;
+			break;
+		case "-":
+			result = val1-val2;
+			break;
+		case "*":
+			result = val1*val2;
+			break;
+		case "/":
+			result = val1/val2;
+			break;
+		}
+		return result;
+	}
 }
+
+
+
+
+
+
+
+
+
+
